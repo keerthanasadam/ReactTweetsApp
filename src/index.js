fd3184a897d757889724d561b4860994a2af6b23
keerthanasadam <keerthana.sadam@gmail.com>
2019-05-19 09:10:29 -0500

    Updates react-scripts and add redux-thunk library

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
-
-import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import reducer from "./reducers/index";
+import App from "./components/App";
+import reducer from "./reducers";
 
 const store = createStore(reducer);
 
