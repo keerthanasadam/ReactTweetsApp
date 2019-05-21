36d725e2be56eba7c408e9a2b3e593d9499d5a9b
keerthanasadam <keerthana.sadam@gmail.com>
2019-05-20 21:49:47 -0500

    Added LoadingBar component

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,16 +1,20 @@
+import { hideLoading, showLoading } from "react-redux-loading";
+
 import { getInitialData } from "../utils/api";
+import { setAuthedUser } from "./authedUser";
 import { receiveTweets } from "./tweets";
 import { receiveUsers } from "./users";
-import { setAuthedUser } from "./authedUser";
 
 const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData() {
   return dispatch => {
+    dispatch(showLoading());
     return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveTweets(tweets));
       dispatch(receiveUsers(users));
       dispatch(setAuthedUser(AUTHED_ID));
+      dispatch(hideLoading());
     });
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,10 @@ import { combineReducers } from "redux";
 import authedUser from "./authedUser";
 import tweets from "./tweets";
 import users from "./users";
-
+import { loadingBarReducer } from "react-redux-loading";
 export default combineReducers({
   tweets,
   users,
-  authedUser
+  authedUser,
+  loadingBar: loadingBarReducer
 });
